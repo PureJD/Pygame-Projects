14404825cdb173d9d854882d5749606032e3baab
Jon <6454jonny@gmail.com>
2024-06-10 12:35:31 +0100

    character and movement added to the project

--- GERM/main.py
+++ GERM/main.py
@@ -1,26 +1,47 @@
 import pygame
+from variables import *
+
+
 
 # Initialize the game   
 pygame.init()
-
-# Set up the screen
-screen = pygame.display.set_mode((2200, 1200))
-
-# Set up the colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-
-# Set up the window name    
+screen = pygame.display.set_mode((WIDTH, HEIGHT)) 
 pygame.display.set_caption("GERM")
+
+class Main_Character:
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def draw(self):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+
+
+main_character = Main_Character(50, 50, 5, 5, RED)
+
+
+
 
 gameloop = True
 while gameloop:
     screen.fill(BLACK)
+    main_character.draw()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameloop = False
+        
+        # movement of the character
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                main_character.x -= 5
+            if event.key == pygame.K_RIGHT:
+                main_character.x += 5
+            if event.key == pygame.K_UP:
+                main_character.y -= 5
+            if event.key == pygame.K_DOWN:
+                main_character.y += 5
 
     pygame.display.update()
---
+++ GERM/variables.py
@@ -0,0 +1,9 @@
+# Set up the colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Set up the screen
+WIDTH, HEIGHT = 2200, 1200
